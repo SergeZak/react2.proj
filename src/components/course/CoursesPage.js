a51d8c054fe8b3c8285a0e5b7aa31ac6edfdd1dc
developer <developer@mail.com>
2017-05-01 14:05:02 +0300

    added components: CourseListRow and CourseList

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,14 +2,13 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
 import {bindActionCreators} from 'redux';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component{
 
   constructor(props, context)
   {
     super(props, context);
-
-    this.courseRow = this.courseRow.bind(this);
   }
 
 
@@ -25,18 +24,14 @@ class CoursesPage extends React.Component{
   }
 
 
-  courseRow(course, index)
-  {
-    return <div key={index}>{course.title}</div>;
-  }
-
-
   render()
   {
+    const {courses} = this.props;
+
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
+        <CourseList courses={courses} />
       </div>
     );
   }
