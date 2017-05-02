98323d19011ca20136cd78b23cfd4991d48f9aff
developer <developer@mail.com>
2017-05-02 11:23:11 +0300

    created saveCourse action

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -6,6 +6,15 @@ export function loadCoursesSuccess(courses)
   return {type: types.LOAD_COURSES_SUCCESS, courses: courses};
 }
 
+export function createCourseSuccess(course)
+{
+  return {type: types.CREATE_COURSE_SUCCESS, course: course};
+}
+
+export function updateCourseSuccess(course)
+{
+  return {type: types.UPDATE_COURSE_SUCCESS, course: course}
+}
 
 
 export function loadCourses()
@@ -20,4 +29,18 @@ export function loadCourses()
         throw(error);
       });
   };
+}
+
+export function saveCourse(course)
+{
+  return function (dispatch, getState)
+  {
+    return courseApi.saveCourse(course)
+      .then(course => {
+        course.id ? dispatch(updateCourseSuccess(course)) : dispatch(createCourseSuccess(course));
+      })
+      .catch(error => {
+        throw(error);
+      });
+  }
 }
